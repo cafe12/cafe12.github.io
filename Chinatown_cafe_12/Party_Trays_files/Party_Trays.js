f7f80d5fbcb7ab223930fdae15552deb1157e97b
erinz <erinuw@gmail.com>
2014-07-29 19:20:33 -0700

    add files

---
+++ Chinatown_cafe_12/Party_Trays_files/Party_Trays.js
@@ -0,0 +1,9 @@
+// Created by iWeb 3.0.3 local-build-20140728
+
+setTransparentGifURL('Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({shadow_0:new IWShadow({blurRadius:10,offset:new IWPoint(4.2426,4.2426),color:'#000000',opacity:0.750000})});registry.applyEffects();}
+function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Party_Trays_files/Party_TraysMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');fixupAllIEPNGBGs();fixAllIEPNGs('Media/transparent.gif');applyEffects()}
---
+++ Chinatown_cafe_12/Welcome_files/Welcome.js
@@ -0,0 +1,9 @@
+// Created by iWeb 3.0.3 local-build-20140728
+
+setTransparentGifURL('Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({shadow_0:new IWShadow({blurRadius:10,offset:new IWPoint(4.2426,4.2426),color:'#000000',opacity:0.750000})});registry.applyEffects();}
+function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Welcome_files/WelcomeMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');fixupAllIEPNGBGs();fixAllIEPNGs('Media/transparent.gif');applyEffects()}
