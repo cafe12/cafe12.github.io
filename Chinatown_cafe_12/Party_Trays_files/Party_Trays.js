76ae97a4c80a6003a4f69b4bf3b40c09b4d0c70f
cafe12 <huang1239@Ivan-Huangs-iMac.local>
2014-08-03 19:44:23 -0700

    Major update Ivan

--- Chinatown_cafe_12/Party_Trays_files/Party_Trays.js
+++
@@ -1,9 +0,0 @@
-// Created by iWeb 3.0.3 local-build-20140730
-
-setTransparentGifURL('Media/transparent.gif');function applyEffects()
-{var registry=IWCreateEffectRegistry();registry.registerEffects({shadow_1:new IWShadow({blurRadius:10,offset:new IWPoint(4.2426,4.2426),color:'#000000',opacity:0.750000}),shadow_0:new IWShadow({blurRadius:10,offset:new IWPoint(4.2426,4.2426),color:'#000000',opacity:0.750000})});registry.applyEffects();}
-function hostedOnDM()
-{return false;}
-function onPageLoad()
-{loadMozillaCSS('Party_Trays_files/Party_TraysMoz.css')
-adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');fixupAllIEPNGBGs();fixAllIEPNGs('Media/transparent.gif');applyEffects()}
